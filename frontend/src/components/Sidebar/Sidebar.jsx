5f36d5d9f1b006fdf0866164c2baddab507b52ac
ProAbenezer <devabenezer7@gmail.com>
2023-11-28 14:58:00 +0100

    modifying the sidebar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -13,22 +13,7 @@ function Sidebar() {
     console.log(topics[0].location)
     return (
         <aside className='sidebar'>
-            <div className="sidebar__content">
-                <h1 className="sidebar__header--title">{topics[0].header}</h1>
-                <ul className="sidebar__topics">
-                    {
-                        topics[0].body.map(topic => <li className='sidebar__topic'><NavLink to={topics[1].location}>{topic}</NavLink></li>)
-                    }
-                </ul>
-            </div>
-            <div className="sidebar__content">
-                <h1 className="sidebar__header--title">{topics[1].header}</h1>
-                <ul className="sidebar__topics">
-                    {
-                        topics[1].body.map(topic => <li className='sidebar__topic'><NavLink to={topics[1].location}>{topic}</NavLink></li>)
-                    }
-                </ul>
-            </div>
+
         </aside>
     )
 }
